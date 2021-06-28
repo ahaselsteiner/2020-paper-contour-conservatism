d5599722c002829cdd233b395b2f2e5fbe2cd04f
Andreas Haselsteiner <a.haselsteiner@uni-bremen.de>
2021-06-28 17:16:55 +0200

    Create figure with multiple response variables effect.

--- motivational-figures/CreateIFORMProblemFigure.m
+++ motivational-figures/CreateIFORMProblemFigure.m
@@ -140,6 +140,80 @@ lg.Layout.Tile = 'north';
 
 exportgraphics(layout, 'IFORM_problem.pdf')
 
+
+% Figure on combining response variables
+fig = figure('position', [100 100 370 340]);
+layout = tiledlayout(1,1);
+nexttile;
+hold on
+
+PM.name = 'V-Hs model from X for dataset D (FINO1)';
+PM.modelType = 'CMA';
+PM.distributions = {'exponentiated-weibull'; 'exponentiated-weibull'};
+PM.isConditionals = {[0 0 0 ]; [1 1 0]};
+PM.coeffs = {
+    {10.0 2.42 0.761}; 
+    { 
+    @(x1) (0.488 + 0.0114 * x1^2.03) / (2.0445^(1 / (0.714 + 1.70 / (1 + exp(-0.304 * (x1 - 8.77))))));
+    @(x1) 0.714 + 1.70 / (1 + exp(-0.304 * (x1 - 8.77)));
+    5}
+    };
+PM.labels = {'Wind speed (m s^{-1})';
+    'Significant wave height (m)'};
+
+hubHeight = 90;
+jointModelHeight = 10;
+alpha = 0.14; % as for normal wind speed in IEC 61400-3-1 p. 35
+v_factor = (hubHeight / jointModelHeight)^alpha;
+v_factor = v_factor * 0.95; % 61400-3-1:2019 p. 34
+
+
+alpha = 1 / (50 * 365.25 * 24);
+[x1Array, x2Array] = computeIformContour(PM, alpha, 180, 0);
+
+h_contour = plot([x1Array x1Array(1)] * v_factor, [x2Array x2Array(1)], ...
+    '-b', 'linewidth', lw, 'DisplayName', 'IFORM contour');
+hold on
+v = 0:0.25:46;
+hs = 0:0.2:20;
+
+[vgrid, hgrid] = meshgrid(v, hs);
+sgrid = 0.012 + 0.021 ./ (1 + exp(-0.3*(vgrid - 10)));
+tgrid = sqrt(2 * pi * hgrid ./ (9.81 * sgrid));
+
+% Response at 30 m water depth
+R = ResponseEmulator;
+rgrid = R.ICDF1hr(vgrid, hgrid, tgrid, 0.5);
+rcap = 301 * 10^6;
+c = contour(vgrid, hgrid, rgrid, [rcap rcap], ':k');
+c = c(:,2:end);
+c = [[v(1); hs(end)] c [v(end); hs(end)]];
+h_30m = patch(c(1,:), c(2,:), [1 0 0], 'FaceAlpha', 0.5, ...
+    'EdgeColor', 'k', 'LineStyle', 'None', ...
+    'DisplayName', 'Failure surface at 30 m water depth');
+
+% Response at 5 m water depth
+R = ResponseEmulator5mWaterDepth;
+rgrid = R.ICDF1hr(vgrid, hgrid, tgrid, 0.5);
+rcap = 105.7 * 10^6;
+c = contour(vgrid, hgrid, rgrid, [rcap rcap], '--k');
+mask = c(1,:) < v(end);
+c = c(:,mask);
+c = [[v(1); hs(end)] c [v(end); hs(1)] [v(end); hs(end)]];
+h_5m = patch(c(1,:), c(2,:), [1 0 0], 'FaceAlpha', 0.5, ...
+    'EdgeColor', 'k', 'LineStyle', 'None', ...
+    'DisplayName', 'Failure surface at 5 m water depth');
+xlabel(PM.labels{1});
+ylabel(PM.labels{2});
+box off
+
+% Create a Legend with the data from multiple axes
+lg = legend(nexttile(1), [h_contour, h_5m, h_30m], ...
+     'Orientation', 'Vertical', 'box', 'off');
+lg.Layout.Tile = 'north';
+
+exportgraphics(layout, 'IFORM_wind_turbine.pdf')
+
 function r = directionalResponse(hx, hy, phi, a, b)
     hxPrime = hx * cos(phi) + hy * sin(phi);
     hyPrime = -1 * hx * sin(phi) + hy * cos(phi);
